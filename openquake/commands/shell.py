7c0e232bd0f5d13f2a334b7e3b6979cc72fee614
Michele Simionato <michele.simionato@gmail.com>
2018-02-01 18:22:19 +0100

    Added oq.status [skip hazardlib]

--- openquake/commands/shell.py
+++ openquake/commands/shell.py
@@ -15,9 +15,22 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+import getpass
 from openquake.baselib import sap
+from openquake.commonlib import logs
 from openquake.server import views
 from openquake.commands import abort
+
+
+def run(job_ini, prev_id=None):
+    """Submit an OpenQuake job and return its ID"""
+    job_id, _pid = views.submit_job(job_ini, getpass.getuser(), prev_id)
+    return job_id
+
+
+def status(job_id):
+    """Check the status of a job"""
+    return logs.dbcmd('calc_info', job_id)
 
 
 class OQ(object):
@@ -36,7 +49,8 @@ class OQ(object):
         self.get_site_collection = readinput.get_site_collection
         self.get_exposure = readinput.get_exposure
         self.make_hmap = calc.make_hmap
-        self.run = views.submit_job
+        self.run = run
+        self.status = status
         self.abort = abort.abort
         # TODO: more utilities when be added when deemed useful
 
